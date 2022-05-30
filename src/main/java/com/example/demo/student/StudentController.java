748280d37fb80f8449ca79a390cece673ebd4692
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2022-05-30 01:30:25 +0100

    fixed api issue. Gneder enum not working correctly.

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -3,6 +3,8 @@ package com.example.demo.student;
 import java.util.Arrays;
 import java.util.List;
 
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
 	
+	
 	@GetMapping
 	public List <Student> getAllStudents(){
 		List<Student> students= Arrays.asList(
-				new Student(1L,"Hello","abc@test.com",Gender.FEMALE),
-				new Student(2L,"Smith","smith@test.com",Gender.MALE),
-				new Student(3L,"David","david@test.com",Gender.OTHER)
+				new Student(1L, "smith", "ter@l.com", null),
+				new Student(2L, "rust", "gh@v.com", null),
+				new Student(3L, "thomas", "abc2@m.com", null)
 		);
 		return students;
 		}
